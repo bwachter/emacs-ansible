9fe963517c83276f09523d7b66e4f6b6ad990867
Mark A. Hershberger <mah@nichework.com>
2025-02-07 22:34:04 -0500

    Update snippets
    * Alter that generate.rb so that it will print out problematic files and continue going.
    * Remove submodule.
    
    See #6

--- lib/generate.rb
+++ lib/generate.rb
@@ -3,12 +3,16 @@ require 'yaml'
 require 'erb'
 
 option_keys = []
+problematic_files = []
+
 Dir::glob(__dir__ + "/ansible/lib/ansible/modules/**/*").each {|f|
   if File.directory? f
     FileUtils.mkdir_p(File.join(__dir__ + "/../snippets/text-mode/ansible", File.basename(f)))
     next
   end
-  puts f
+
+  puts "Processing: #{f}"
+
   yml = ''
   start = false
   open(f).each {|line|
@@ -19,7 +23,17 @@ Dir::glob(__dir__ + "/ansible/lib/ansible/modules/**/*").each {|f|
     start = true if /^DOCUMENTATION = .?'''/.match line
     start = true if /^DOCUMENTATION = .?"""/.match line
   } if File.file? f
-  doc = YAML.load(yml)
+
+  begin
+    doc = YAML.safe_load(yml)
+  rescue Psych::SyntaxError => e
+    relative_path = f
+    relative_path.sub(__dir__ + "/ansible/lib/ansible/modules", "")
+    msg = "Error parsing YAML in #{relative_path}: #{e.message}"
+    puts msg
+    problematic_files << msg
+    next
+  end
   next unless doc
   index = 2
   options = ''
@@ -53,7 +67,15 @@ Dir::glob(__dir__ + "/ansible/lib/ansible/modules/**/*").each {|f|
 EOS
   snippet = ERB.new template
   dirname = File.basename(File.dirname(f))
-  File.write(File.join(__dir__ + "/../snippets/text-mode/ansible", dirname, File.basename(f).gsub(/.py$/, '')), snippet.result(binding))
+  snippet_dir = File.join(__dir__ + "/../snippets/text-mode/ansible", dirname)
+  FileUtils.mkdir_p(snippet_dir) unless Dir.exist?(snippet_dir)
+  File.write(File.join(snippet_dir, File.basename(f).gsub(/.py$/, '')), snippet.result(binding))
 
   File.write(__dir__ + "/../dict/ansible", option_keys.uniq.join("\n"))
 }
+
+# Print the list of problematic files at the end
+if problematic_files.any?
+  puts "\nList of problematic files:"
+  problematic_files.each { |file| puts file }
+end
